4e4601db96959c522f1bd0e5a9d0ba882b165563
Sebastián da Silva <sebastiandasilva@192.168.1.19>
2025-10-30 22:22:42 -0300

    renames collab

---
+++ Test_Git.py
@@ -0,0 +1,10 @@
+print("Hello from VS Code on Mac")
+
+def test_function():
+    return "Original version"
+
+print(test_function())
+# define a function for ar olling simple averge mean
+
+def average(lst):
+    return sum(lst) / len(lst)
